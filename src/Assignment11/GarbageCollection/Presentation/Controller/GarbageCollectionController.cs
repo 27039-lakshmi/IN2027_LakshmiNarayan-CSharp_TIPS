056133f0adec8d489af85fdff4777cc21013caad
27039-lakshmi <lakshmi.narayan@solitontech.com>
2026-09-04 17:26:02 +0530

    feat: complete all task

---
+++ src/Assignment11/GarbageCollection/Presentation/Controller/GarbageCollectionController.cs
@@ -0,0 +1,35 @@
+﻿using GarbageCollection.Application.Service;
+
+namespace GarbageCollection.Presentation.Controller
+{
+    /// <summary>
+    /// Coordinates the execution of the garbage collection demonstration.
+    /// </summary>
+    public class GarbageCollectionController
+    {
+        private readonly GarbageCollectionService _service;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="GarbageCollectionController"/> class.
+        /// </summary>
+        /// <param name="service">
+        /// The service responsible for creating and releasing memory allocations.
+        /// </param>
+        public GarbageCollectionController(GarbageCollectionService service)
+        {
+            this._service = service;
+        }
+
+        /// <summary>
+        /// Starts the garbage collection demonstration workflow.
+        /// </summary>
+        public void Start()
+        {
+            Console.ReadKey(); // used for breakpoint
+            this._service.CreateAndDestroyMemory();
+            GC.Collect();
+            Console.ReadKey(); // used for breakpoint
+        }
+    }
+}
---
+++ src/Assignment11/IDisposableDemo/Presentation/Controller/IDisposableController.cs
@@ -0,0 +1,28 @@
+﻿using IDisposableDemo.Application.Service;
+
+namespace IDisposableDemo.Presentation.Controller
+{
+    /// <summary>
+    /// Coordinates user interaction for the IDisposable demonstration,
+    /// including writing data to a file and reading it back.
+    /// </summary>
+    public class IDisposableController
+    {
+        /// <summary>
+        /// Starts the IDisposable demonstration workflow.
+        /// </summary>
+        public void Start()
+        {
+            FileWriter service;
+            using (service = new FileWriter("C:/chummah/data.txt"))
+            {
+                Console.WriteLine("Enter text to write into file");
+                service.WriteIntoFile(Console.ReadLine());
+            }
+
+            Console.WriteLine("Data in file");
+            string? text = service.ReadFromFile();
+            Console.WriteLine(text);
+        }
+    }
+}
