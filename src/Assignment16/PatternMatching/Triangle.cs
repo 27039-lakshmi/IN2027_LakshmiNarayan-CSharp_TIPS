1fdc5bc45755dee1c524fe9ad131565965092aca
27039-lakshmi <lakshmi.narayan@solitontech.com>
2026-09-16 20:10:42 +0530

    feat: complete task7

---
+++ src/Assignment16/PatternMatching/Triangle.cs
@@ -0,0 +1,44 @@
+﻿namespace PatternMatching
+{
+    /// <summary>
+    /// Represents a triangle shape and provides
+    /// functionality to calculate its area.
+    /// </summary>
+    public class Triangle : Shape
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Triangle"/> class.
+        /// </summary>
+        /// <param name="color">Color of the shape</param>
+        /// <param name="shape">Shape name of the shape</param>
+        /// <param name="baseOfTriangle">Base value of the triangle</param>
+        /// <param name="heightOfTriangle">Height value of the rectangle</param>
+        public Triangle(string color, string shape, int baseOfTriangle, int heightOfTriangle)
+            : base(color, shape)
+        {
+            this.Base = baseOfTriangle;
+            this.Height = heightOfTriangle;
+        }
+
+        /// <summary>
+        /// Gets or sets the base length of the triangle.
+        /// </summary>
+        /// <value>Base of the triangle</value>
+        public int Base { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height of the triangle.
+        /// </summary>
+        /// <value>Height of the triangle</value>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Calculates the area of the triangle.
+        /// </summary>
+        /// <returns>The area of the triangle.</returns>
+        public int CalculateArea()
+        {
+            return this.Base * this.Height;
+        }
+    }
+}
