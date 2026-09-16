203fb3fef44d78dfe28180d0031efa1202ff1824
27039-lakshmi <lakshmi.narayan@solitontech.com>
2026-09-16 13:13:09 +0530

    feat: complete task2

---
+++ VarAndDynamicDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignments
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var varTypeVariable = "Hello";
+            dynamic dynamicTypeVariable = "Hello";
+            Console.WriteLine("Value of var type variable " + varTypeVariable);
+            Console.WriteLine("Value of dynamic type variable before change " + dynamicTypeVariable);
+
+            // varTypeVariable = 9; -> Cannot change datatype of a variable declared with var keyword.
+            dynamicTypeVariable = 9;
+            Console.WriteLine("Value of dynamic type variable after change " + dynamicTypeVariable);
+        }
+    }
+}
