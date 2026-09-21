5abbadb68d35bd84b0b7e0693a7a462055a0db0a
27039-lakshmi <lakshmi.narayan@solitontech.com>
2026-09-21 18:40:24 +0530

    fix: add try block

--- src/Assignment16/RecordsDemo/Book.cs
+++ src/Assignment16/RecordsDemo/Book.cs
@@ -1,4 +1,10 @@
 ﻿namespace RecordsDemo
 {
+    /// <summary>
+    /// Represents a book with a title, author name, and ISBN number.
+    /// </summary>
+    /// <param name="title">The title of the book.</param>
+    /// <param name="authorName">The name of the author.</param>
+    /// <param name="isbn">The ISBN number of the book.</param>
     public record Book(string title, string authorName, int isbn);
 }
